33054f446cc10fc695bd49d98f266b1709a8df3a
kth-xx <kevinheavey@gmail.com>
2023-06-12 16:58:36 +0100

    Update Program.cs

--- Cinegy.TsAnalyzer/Program.cs
+++ Cinegy.TsAnalyzer/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright 2016-2023 Cinegy GmbH. KEV eddition!
+﻿/* Copyright 2016-2023 Cinegy GmbH.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
